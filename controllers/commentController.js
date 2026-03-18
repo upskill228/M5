37962b786a9011b04e5f932060b5684ace0751c8
upskill228 <upskill228@alunos.ciencias.ulisboa.pt>
2026-03-18 09:22:26 +0000

    aula - quarta 1

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,5 +1,16 @@
 import * as commentService from "../services/commentService.js";
 
+// GET
+export const getCommentsByTaskId = (req, res) => {
+  try {
+    const comments = commentService.getCommentsByTaskId(req.params.id);
+    res.json(comments);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+// POST
 export const createComment = (req, res) => {
   try {
     const comment = commentService.createComment(req.params.id, req.body);
